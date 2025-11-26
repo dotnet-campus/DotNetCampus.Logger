e9ab59723a0a9948504ffe01b48f764436d3f129
walterlv <xblero@gmail.com>
2025-11-26 10:18:25 +0800

    修复日志等级记录错误

--- src/DotNetCampus.Logger/LoggerExtensions.g.cs
+++ src/DotNetCampus.Logger/LoggerExtensions.g.cs
@@ -69,7 +69,7 @@ public static class LoggerExtensions
     /// <param name="exception">如果有异常信息，可以传入此参数。</param>
     public static void Error(this ILogger logger, string message, Exception? exception = null)
     {
-        logger.Log(LogLevel.Warning, default, message, null, (s, ex) => message);
+        logger.Log(LogLevel.Error, default, message, null, (s, ex) => message);
     }
 
     /// <summary>
